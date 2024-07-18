b3e7e06f0b79e4f38e7d853f4711fdf5bd27b35d
rohitbharti279 <rohitbharti279@gmail.com>
2024-07-18 19:08:58 +0530

    WIP: resolve existing errors and attempt to fix new issues

--- test/StableBaseCDP.js
+++ test/StableBaseCDP.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+// const ethers = require("hardhat");
 
 describe("StableBaseCDP", function () {
   let stableBaseCDP, owner, addr1;
@@ -9,20 +10,25 @@ describe("StableBaseCDP", function () {
     [owner, addr1] = await ethers.getSigners();
     const StableBaseCDPFactory = await ethers.getContractFactory("StableBaseCDP");
     stableBaseCDP = await StableBaseCDPFactory.deploy();
-    await stableBaseCDP.deployed();
+    // await stableBaseCDP.deployed();
+    await stableBaseCDP.waitForDeployment();
   });
 
   // Test case for opening a new safe with ETH
   it("should open a new safe with ETH", async function () {
-    const depositAmount = ethers.utils.parseEther("1"); // Amount of ETH to deposit
-    const reserveRatio = 150; // Reserve ratio for the safe
+    const depositAmount = ethers.parseEther("1"); // Amount of ETH to deposit
+    const reserveRatio = 100; // Reserve ratio for the safe
+    console.log("ethers:-> ", ethers);
 
     // Open a safe with ETH
-    await stableBaseCDP.connect(addr1).openSafe(ethers.constants.AddressZero, depositAmount, reserveRatio, { value: depositAmount });
+    await stableBaseCDP.connect(addr1).openSafe(ethers.ZeroAddress, depositAmount, reserveRatio, { value: depositAmount });
 
     // Compute the safe ID
-    const safeId = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode([ "address", "address" ], [ addr1.address, ethers.constants.AddressZero ]));
+    const safeId = ethers.keccak256(ethers.AbiCoder.defaultAbiCoder().encode([ "address", "address" ], [ addr1.address, ethers.ZeroAddress ]));
     const safe = await stableBaseCDP.safes(safeId);
+    console.log("safe:-> ", safe);
+    console.log("depositAmount:->", depositAmount);
+    console.log("safe.depositedAmount:->", safe.depositedAmount);
 
     // Check if the safe has the correct deposited amount and reserve ratio
     expect(safe.depositedAmount).to.equal(depositAmount);
@@ -31,19 +37,20 @@ describe("StableBaseCDP", function () {
 
   // Test case for opening a new safe with ERC20 token
   it("should open a new safe with ERC20 token", async function () {
-    const ERC20Mock = await ethers.getContractFactory("ERC20Mock");
-    const token = await ERC20Mock.deploy("Mock Token", "MKT", ethers.utils.parseEther("1000"));
-    await token.deployed();
+    const ERC20Token = await ethers.getContractFactory("ERC20Token");
+    const token = await ERC20Token.deploy("Mock Token", "MKT", ethers.parseEther("1000"));
+    await token.waitForDeployment(); // Await deployment
 
-    const depositAmount = ethers.utils.parseEther("100"); // Amount of ERC20 token to deposit
-    const reserveRatio = 150; // Reserve ratio for the safe
+    const depositAmount = ethers.parseEther("100"); // Amount of ERC20 token to deposit
+    const reserveRatio = 100; // Reserve ratio for the safe
 
     // Approve the token transfer and open a safe with the ERC20 token
+    console.log("depositAmount:-> ", depositAmount);
     await token.connect(addr1).approve(stableBaseCDP.address, depositAmount);
     await stableBaseCDP.connect(addr1).openSafe(token.address, depositAmount, reserveRatio);
 
     // Compute the safe ID
-    const safeId = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode([ "address", "address" ], [ addr1.address, token.address ]));
+    const safeId = ethers.keccak256(ethers.AbiCoder.defaultAbiCoder().encode([ "address", "address" ], [ addr1.address, token.address ]));
     const safe = await stableBaseCDP.safes(safeId);
 
     // Check if the safe has the correct deposited amount and reserve ratio
@@ -53,14 +60,14 @@ describe("StableBaseCDP", function () {
 
   // Test case for closing a safe and returning the collateral
   it("should close a safe and return the collateral", async function () {
-    const depositAmount = ethers.utils.parseEther("1"); // Amount of ETH to deposit
-    const reserveRatio = 150; // Reserve ratio for the safe
+    const depositAmount = ethers.parseEther("1"); // Amount of ETH to deposit
+    const reserveRatio = 100; // Reserve ratio for the safe
 
     // Open a safe with ETH
-    await stableBaseCDP.connect(addr1).openSafe(ethers.constants.AddressZero, depositAmount, reserveRatio, { value: depositAmount });
+    await stableBaseCDP.connect(addr1).openSafe(ethers.ZeroAddress, depositAmount, reserveRatio, { value: depositAmount });
 
     // Compute the safe ID
-    const safeId = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode([ "address", "address" ], [ addr1.address, ethers.constants.AddressZero ]));
+    const safeId = ethers.keccak256(ethers.AbiCoder.defaultAbiCoder().encode([ "address", "address" ], [ addr1.address, ethers.ZeroAddress ]));
     await stableBaseCDP.connect(addr1).closeSafe(safeId);
 
     // Check if the safe has been closed (deposited amount should be 0)
