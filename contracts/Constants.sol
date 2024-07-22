3bd8e6da49cfd11a7f797593a91cc5272e809e1f
svylabs <sg@svylabs.com>
2024-07-22 18:10:32 +0200

    Adding some math utilities

---
+++ contracts/Constants.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.20;
+
+contract Constants {
+    uint256 public constant TOTAL_MINTED_SBD_IN_BOOTSTRAP_BEFORE_NORMAL = 10 ** 7;
+}
--- contracts/Structures.sol
+++ contracts/Structures.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "./library/Math.sol";
+
 library SBStructs {
     struct Safe {
         address token;
@@ -8,5 +10,16 @@ library SBStructs {
         uint256 borrowedAmount;
         uint256 reserveRatio;
         uint256 originationFeePaid;
+    }
+
+    enum Mode {
+        BOOTSTRAP,
+        NORMAL
+    }
+
+    struct GlobalVars {
+        uint256 totalMintedSBD;
+        Math.Rate referenceOriginationFeeRate;
+        Math.Rate referenceReserveRatio;
     }
 }
