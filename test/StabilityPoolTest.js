241c555cd6a0b64528922b5c3a4772819e06d009
svylabs <sg@svylabs.com>
2024-09-17 03:14:02 +0200

    Updating tests- though they are failing

--- test/StabilityPoolTest.js
+++ test/StabilityPoolTest.js
@@ -132,4 +132,103 @@ describe("StabilityPool", function () {
       expect(globalSnapshot.collateralRewardPerShare).to.equal(ethers.parseEther("0.2"));
     });
   });
+
+  describe("Dynamic staking and rewards", function () {
+    it("Should correctly distribute rewards with dynamic staking and unstaking", async function () {
+      // User1 stakes 100 SBD
+      await expect(stabilityPool.connect(user1).stake(ethers.parseEther("100")))
+        .to.not.emit(stabilityPool, "RewardPaid");
+
+      // Add 50 SBD as rewards
+      await stabilityPool.connect(owner).addRewards(ethers.parseEther("50"));
+      await sbdToken.connect(owner).transfer(stabilityPool.target, ethers.parseEther("50"));
+
+      // User2 stakes 200 SBD
+      await expect(stabilityPool.connect(user2).stake(ethers.parseEther("200")))
+        .to.not.emit(stabilityPool, "RewardPaid");
+
+      // Add 90 SBD as rewards
+      await stabilityPool.connect(owner).addRewards(ethers.parseEther("90"));
+      await sbdToken.connect(owner).transfer(stabilityPool.target, ethers.parseEther("90"));
+
+      // User1 unstakes 50 SBD
+      await expect(stabilityPool.connect(user1).unstake(ethers.parseEther("50")))
+        .to.emit(stabilityPool, "RewardPaid")
+        .withArgs(user1.address, ethers.parseEther("55"), 0);
+
+      // Add 60 SBD as rewards
+      await stabilityPool.connect(owner).addRewards(ethers.parseEther("60"));
+      await sbdToken.connect(owner).transfer(stabilityPool.target, ethers.parseEther("60"));
+
+      // Check final rewards for User1
+      await expect(stabilityPool.connect(user1).withdrawRewards())
+        .to.emit(stabilityPool, "RewardPaid")
+        .withArgs(user1.address, ethers.parseEther("15"), 0);
+
+      // Check final rewards for User2
+      await expect(stabilityPool.connect(user2).withdrawRewards())
+        .to.emit(stabilityPool, "RewardPaid")
+        .withArgs(user2.address, ethers.parseEther("130"), 0);
+
+      // Verify final staked amounts
+      expect(await stabilityPool.getUserStakedAmount(user1.address)).to.equal(ethers.parseEther("50"));
+      expect(await stabilityPool.getUserStakedAmount(user2.address)).to.equal(ethers.parseEther("200"));
+      expect(await stabilityPool.getTotalStaked()).to.equal(ethers.parseEther("250"));
+    });
+
+    it("Should handle multiple users staking, unstaking, and claiming rewards", async function () {
+      const user3 = addrs[0];
+      await sbdToken.mint(user3.address, ethers.parseEther("1000"));
+      await sbdToken.connect(user3).approve(stabilityPool.target, ethers.MaxUint256);
+
+      // Initial stakes
+      await expect(stabilityPool.connect(user1).stake(ethers.parseEther("100")))
+        .to.not.emit(stabilityPool, "RewardPaid");
+      await expect(stabilityPool.connect(user2).stake(ethers.parseEther("200")))
+        .to.not.emit(stabilityPool, "RewardPaid");
+      await expect(stabilityPool.connect(user3).stake(ethers.parseEther("300")))
+        .to.not.emit(stabilityPool, "RewardPaid");
+
+      // Add rewards
+      await stabilityPool.connect(owner).addRewards(ethers.parseEther("120"));
+      await sbdToken.connect(owner).transfer(stabilityPool.target, ethers.parseEther("120"));
+
+      // User2 unstakes half
+      await expect(stabilityPool.connect(user2).unstake(ethers.parseEther("100")))
+        .to.emit(stabilityPool, "RewardPaid")
+        .withArgs(user2.address, ethers.parseEther("40"), 0);
+
+      // Add more rewards
+      await stabilityPool.connect(owner).addRewards(ethers.parseEther("90"));
+      await sbdToken.connect(owner).transfer(stabilityPool.target, ethers.parseEther("90"));
+
+      // User1 stakes more
+      await expect(stabilityPool.connect(user1).stake(ethers.parseEther("50")))
+        .to.emit(stabilityPool, "RewardPaid")
+        .withArgs(user1.address, ethers.parseEther("30"), 0);
+
+      // Add final rewards
+      await stabilityPool.connect(owner).addRewards(ethers.parseEther("60"));
+      await sbdToken.connect(owner).transfer(stabilityPool.target, ethers.parseEther("60"));
+
+      // Check final rewards
+      await expect(stabilityPool.connect(user1).withdrawRewards())
+        .to.emit(stabilityPool, "RewardPaid")
+        .withArgs(user1.address, ethers.parseEther("25"), 0);
+
+      await expect(stabilityPool.connect(user2).withdrawRewards())
+        .to.emit(stabilityPool, "RewardPaid")
+        .withArgs(user2.address, ethers.parseEther("30"), 0);
+
+      await expect(stabilityPool.connect(user3).withdrawRewards())
+        .to.emit(stabilityPool, "RewardPaid")
+        .withArgs(user3.address, ethers.parseEther("145"), 0);
+
+      // Verify final staked amounts
+      expect(await stabilityPool.getUserStakedAmount(user1.address)).to.equal(ethers.parseEther("150"));
+      expect(await stabilityPool.getUserStakedAmount(user2.address)).to.equal(ethers.parseEther("100"));
+      expect(await stabilityPool.getUserStakedAmount(user3.address)).to.equal(ethers.parseEther("300"));
+      expect(await stabilityPool.getTotalStaked()).to.equal(ethers.parseEther("550"));
+    });
+  });
 });
