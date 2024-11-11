fbbe147ed3bf1625013b865a274b7c98b18b10a9
rohitbharti279 <rohitbharti279@gmail.com>
2024-11-11 15:43:21 +0530

    modify

--- contracts/StableBase.sol
+++ contracts/StableBase.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./library/Math.sol";
 import "./interfaces/IPriceOracle.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,30 +1,3 @@
-// /** @type import('hardhat/config').HardhatUserConfig */
-// require("@nomicfoundation/hardhat-toolbox");
-// require("@nomiclabs/hardhat-solhint");
-
-// module.exports = {
-//   solidity: {
-//     version: "0.8.20",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200,
-//       },
-//       viaIR: true,
-//     },
-//   },
-//   networks: {
-//     localhost: {
-//       url: "http://127.0.0.1:8545",  // URL of the local Ethereum node
-//       accounts: {
-//         mnemonic: "test test test test test test test test test test test junk",  // Default Hardhat mnemonic
-//       },
-//     },
-//   },
-// };
-
-
-
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-solhint");
 require("dotenv").config();
