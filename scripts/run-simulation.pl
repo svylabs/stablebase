53508cb4fabb9c3ca37aeb99f6e41ca29e41f444
svylabs <sg@svylabs.com>
2024-11-15 23:46:16 +0100

    Adding ABIs

---
+++ scripts/run-simulation.pl
@@ -0,0 +1,7 @@
+for my $i (40..140) {
+  my $start = time();
+  print(localtime(time())." Running simulation $i\n");
+  `npx hardhat test --config hardhat.config.simulation.js  scripts/simulate.js > simulation-logs/$i.log 2>&1`;
+  my $elapsed = (time() - $start) / 60;
+  print ("Elapsed: $elapsed mins\n");
+}
